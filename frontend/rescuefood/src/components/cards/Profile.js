1cc26461f02ffb022a0f2620be6530f01f73e7c6
sh78ba <sh78ba@gmail.com>
2024-12-06 01:32:27 +0530

    added profile page and made it resposive

--- frontend/rescuefood/src/components/cards/Profile.js
+++ frontend/rescuefood/src/components/cards/Profile.js
@@ -7,9 +7,10 @@ const Profile = () => {
         <div className='text-center content-center'>
         <img className=' rounded-full mx-auto my-3' src={reslogo} alt='logo'/>
         <h1 className='text-3xl'>Restaurant Name</h1>
+        <h1 className='text-3xl'>#AB68278219BC</h1>
         </div>
 
-        <div className='text-2xl'>
+        <div className='text-2xl mt-5'>
         <p>NIT Warangal Road, Hanamkonda, 506004</p>
         <p>Kyc Staus : Done</p>
         <p>GST No : 18KA913H08002001SJ</p>
