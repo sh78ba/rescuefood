ed0888b81827f1b17c4022d64a519de58dbe6d97
sh78ba <sh78ba@gmail.com>
2024-12-12 00:23:30 +0530

    adding password reset functionality

---
+++ backend/configs/config.js
@@ -0,0 +1,1 @@
+export const volunteerResetPasswordURL="http://localhost:6000/rescuefood/api/v1/volunteer/resetpassword"
--- backend/controllers/Auth.controller.js
+++ backend/controllers/Auth.controller.js
@@ -3,6 +3,8 @@ const volunteer_model=require("../models/Volunteer.model")
 const restaurant_model=require("../models/Restaurant.model")
 const bcrypt=require("bcryptjs")
 const jwt=require("jsonwebtoken")
+const config=require("../configs/config")
+const nodemailer=require("nodemailer")
 
 
 /***********For Volunteer  **********/
@@ -63,6 +65,49 @@ exports.volunteersignin=async(req,res)=>{
 }
 
 //volunteer forgot password
+
+exports.volunteerRequestPasswordReset=async(req,res,next)=>{
+    const req_email=req.body.email;
+try{
+    const get_User=await volunteer_model.findOne({email:req_email})
+    if(!get_User){
+        return res.status(404).send({
+            message:"Email doesn't exists!"
+        })
+    }
+    const secret=process.env.SECRET_JWT+get_User.email;
+
+    const token=jwt.sign({email:get_User.email},secret,{expiresIn:'1h'});
+
+    const resetURL=config.volunteerResetPasswordURL+`?email=${get_User.email}&token=${token}`;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 't1129172@gmail.com',
+          pass: 'password',
+        },
+      });
+  
+      const mailOptions = {
+        to: user.email,
+        from: process.env.EMAIL,
+        subject: 'Password Reset Request',
+        text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
+        Please click on the following link, or paste this into your browser to complete the process:\n\n
+        ${resetURL}\n\n
+        If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+      };
+  
+      await transporter.sendMail(mailOptions);
+  
+      res.status(200).json({ message: 'Password reset link sent' });
+    } catch (error) {
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+
+
+}
 
 
 //volunteer location update
